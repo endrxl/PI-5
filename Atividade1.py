b39c2d0b1fc1835782ef59cae09c1597dd47e418
endrxl <gabriel.endrel@escolar.ifrn.edu.br>
2026-05-29 19:53:21 -0300

    Atualização do código

--- Atividade1.py
+++ Atividade1.py
@@ -1,5 +1,6 @@
 class Tarefa:
     tarefas = []
+
     def __init__(self, titulo, descricao):
         self.titulo = titulo
         self.descricao = descricao
@@ -8,17 +9,20 @@ class Tarefa:
 
     def __str__(self):
         return f'{self.titulo} | {self.descricao}'
-    
+
     @classmethod
     def listarTarefas(cls):
         for tarefa in cls.tarefas:
             print(tarefa)
+
+    @classmethod
+    def quantidadeTarefas(cls):
+        return len(cls.tarefas)
+
 
 tarefa1 = Tarefa('Criar metodo', 'como criar metodo')
 tarefa2 = Tarefa('Criar classe', 'como criar classe')
 
 Tarefa.listarTarefas()
 
-qtd_tarefas = len(Tarefa.tarefas)
-
-print(f'A quantidade de tarefas é {qtd_tarefas}')
+print(f'A quantidade de tarefas é {Tarefa.quantidadeTarefas()}')
